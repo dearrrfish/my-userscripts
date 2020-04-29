7791feae2aadb14b4a9c486e511ad6fc0d88c870
Yu Jin <dearrrfish@gmail.com>
2020-04-29 14:41:06 -0700

    feat($spotify-web-sidebar-toggler): renamed filename; replaced animation; rearranged now playing bar elements in narrow view

--- spotify-web-sidebar-toggler.user.js
+++ spotify-web-sidebar-toggler.user.js
@@ -2,7 +2,7 @@
 // @name         Spotify Web Sidebar Toggler
 // @description  Adds the ability to toggle the main sidebar on Spotify Web using a keyboard shortcut (ctrl + alt + B), original code from https://github.com/dumptyd/slack-sidebar-toggler
 // @author       dearrrfish (http://github.com/dearrrfish)
-// @version      1.1.0
+// @version      1.2.0
 // @namespace    http://github.com/dearrrfish
 // @include      https://open.spotify.com/*
 // @grant        GM_addStyle
@@ -55,36 +55,90 @@
 
   const style = {
     leftSidebarCollapsedClassName: 'SST-left-sidebar-collapsed',
-    leftSidebarWidth: '230px',
+    containerSelector: '.Root__top-container',
     navbarSelector: '.Root__nav-bar',
     mainviewSelector: '.Root__main-view',
     topbarSelector: '.Root__top-bar header',
-    buttonSelector: '.Root__top-bar header button[title="Go back"]'
+    nowplayingbarSelector: '.Root__now-playing-bar footer .now-playing-bar',
   };
   GM_addStyle(`
-    .${style.leftSidebarCollapsedClassName} ${style.topbarSelector} {
-      max-width: 100vw;
-    }
-    .${style.leftSidebarCollapsedClassName} ${style.navbarSelector} {
-      width: 0;
-      transform: translate(-${style.leftSidebarWidth});
-    }
-    ${style.navbarSelector} {
-      transition: .2s transform;
-    }
-    ${style.mainviewSelector} > div.nav-bar-toggler {
-      position: absolute;
-      left: 0;
-      top: 0;
-      bottom: 0;
-      width: 7px;
-      height: 100%;
-      display: block;
-    }
-    ${style.mainviewSelector} > div.nav-bar-toggler:hover {
-      content: linear-gradient(#e66465, #9198e5);
-    }
+
+.${style.leftSidebarCollapsedClassName} ${style.topbarSelector} {
+  max-width: 100vw;
+}
+
+${style.containerSelector} {
+  transition: .2s;
+}
+
+.${style.leftSidebarCollapsedClassName} ${style.navbarSelector} {
+  width: 0;
+  opacity: 0;
+  transition: width .2s, opacity .2s;
+}
+
+${style.mainviewSelector} {
+  transition: width .2s;
+}
+
+${style.mainviewSelector} > div.nav-bar-toggler {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  width: 7px;
+  height: 100%;
+  display: block;
+}
+${style.mainviewSelector} > div.nav-bar-toggler:hover {
+  cursor: e-resize;
+  content: linear-gradient(#e66465, #9198e5);
+}
   `);
+
+  GM_addStyle(`
+body {
+  font-family: Microsoft Yahei;
+}
+
+@media screen and (max-width: 700px) {
+  body {
+    min-width: unset;
+  }
+
+  ${style.nowplayingbarSelector} {
+    width: 100%;
+    min-width: unset;
+    max-width: 100vw;
+    min-height: 70px;
+    padding: 10px 20px;
+    height: unset;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+  ${style.nowplayingbarSelector} .now-playing-bar__left {
+      width: auto;
+      order: 1;
+  }
+
+  ${style.nowplayingbarSelector} .now-playing-bar__center {
+      width: 100%;
+      order: 3;
+  }
+
+  ${style.nowplayingbarSelector} .now-playing-bar__right {
+      max-width: 25%;
+      min-width: unset;
+      order: 2;
+  }
+
+  ${style.nowplayingbarSelector} .now-playing-bar__right__inner {
+      width: 100%;
+  }
+}
+  `)
 
 
   function toggleSideBar() {
